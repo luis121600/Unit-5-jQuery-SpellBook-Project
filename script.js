c5c16fa8f1e6b8e998b7d58c92f19f0ed18f60cb
luis121600 <49485657+luis121600@users.noreply.github.com>
2019-04-10 09:33:30 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("img").click(function(){
+ $("#Thanos").fadeOut();
+});
+
+
+
+//Spell #2
+
+$("img").click(function(){
+ $("#DoctorStrange").slideUp();
+});
+
+
+
+//Spell #3
+
+$("   img   ").click(function(){
+ $("#Spiderman").hide();
+});
+
+
+
